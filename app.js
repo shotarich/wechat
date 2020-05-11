c50be3a55d8636f6b217d54546060e5223ddc3ab
lsdong <shotarich@163.com>
2020-05-11 22:53:37 +0800

    引入ejs模板引擎

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const Koa = require('koa')
+const path = require('path')
+const views =  require('koa-views')
 const Menu = require('./wechat/menu/Menu')
 const AuotReply = require('./wechat/reply/Reply')
 const parseWechatReq = require('./libs/parseWechatReq')
@@ -7,6 +9,19 @@ const validWechatAccess = require('./libs/validWechatAccess')
 const app = new Koa()
 const autoReply = new AuotReply()
 const menu = new Menu()
+
+// 加载模板引擎
+app.use(views(path.join(__dirname, './views'), {
+  extension: 'ejs'
+}))
+
+app.use(async (ctx, next) => {
+  if(ctx.originalUrl === '/wechat') {
+    await ctx.render('index')
+  }else {
+    await next()
+  }
+})
 
 app.use(validWechatAccess)
 app.use(async (ctx, next) => {
