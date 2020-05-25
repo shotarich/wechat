090992376cf859916f84c1f038367646f5502a8d
lsdong <shotarich@163.com>
2020-05-25 21:09:24 +0800

    优化

--- wechat/menu/menuConf.js
+++ wechat/menu/menuConf.js
@@ -1,24 +1,24 @@
 exports = module.exports = {
   button: [
     {	
-      name: "今日歌曲",
-      type: "click",
-      key: "V1001_TODAY_MUSIC"
+      name: '撩一下',
+      type: 'click',
+      url: 'https://www.meets.love/wechat/'
     },
-    {
-      name: "菜单",
-      sub_button: [
-        {
-          "name":"搜索",
-          "type":"view",
-          "url":"http://www.soso.com/"
-        },
-        {
-          "name":"赞一下我们",
-          "type":"click",
-          "key":"V1001_GOOD"
-        }
-      ]
-    }
+    // {
+    //   name: "菜单",
+    //   sub_button: [
+    //     {
+    //       "name":"搜索",
+    //       "type":"view",
+    //       "url":"http://www.soso.com/"
+    //     },
+    //     {
+    //       "name":"赞一下我们",
+    //       "type":"click",
+    //       "key":"V1001_GOOD"
+    //     }
+    //   ]
+    // }
   ]
 }
